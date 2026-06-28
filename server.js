70d4b2f0ce16a0ec60de46efbf08febbee94d7d7
Leo <95978312+yaefdb@users.noreply.github.com>
2026-06-29 01:29:33 +0800

    Serve static assets via Vercel CDN, add gzip compression

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const cors = require("cors");
+const compression = require("compression");
 const path = require("path");
 require("dotenv").config();
 
 const app = express();
 
+app.use(compression());
 app.use(cors());
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "public")));
