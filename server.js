19eea0c3e3bda7810c859e380c4a5ae7ecc95d95
Leo <95978312+yaefdb@users.noreply.github.com>
2026-06-29 00:41:28 +0800

    Serve static files and HTML views

--- server.js
+++ server.js
@@ -2,10 +2,23 @@ const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
 
+const path = require("path");
+
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", (req, res) => res.sendFile(path.join(__dirname, "views", "index.html")));
+app.get("/login", (req, res) => res.sendFile(path.join(__dirname, "views", "login.html")));
+app.get("/register", (req, res) => res.sendFile(path.join(__dirname, "views", "register.html")));
+app.get("/order", (req, res) => res.sendFile(path.join(__dirname, "views", "order.html")));
+app.get("/reserve", (req, res) => res.sendFile(path.join(__dirname, "views", "reserve.html")));
+app.get("/rewards", (req, res) => res.sendFile(path.join(__dirname, "views", "rewards.html")));
+app.get("/profile", (req, res) => res.sendFile(path.join(__dirname, "views", "profile.html")));
+app.get("/qrpage", (req, res) => res.sendFile(path.join(__dirname, "views", "qrpage.html")));
+app.get("/forgotpassword", (req, res) => res.sendFile(path.join(__dirname, "views", "forgotpassword.html")));
 
 // Routes
 app.use("/auth", require("./routes/auth"));
