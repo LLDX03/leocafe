0e304e0a17a8cc5a21ed14eeaade20991262c30d
Leo <95978312+yaefdb@users.noreply.github.com>
2026-06-29 01:30:45 +0800

    Revert static CDN routing, serve via Express with cache headers

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(compression());
 app.use(cors());
 app.use(express.json());
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "public"), { maxAge: "7d" }));
 
 // Page routes
 app.get("/", (req, res) => res.sendFile(path.join(__dirname, "views", "login.html")));
